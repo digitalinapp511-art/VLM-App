6d4777f3450037ed9fb66e59210730a1417a951b
Ritik Parihar <ritikparihar2040@gmail.com>
2026-06-25 23:28:49 +0530

    chore: remove backend from git tracking

--- backend/src/utils/generateToken.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (phone: string) => {
-    return jwt.sign(
-        { phone },
-        process.env.JWT_SECRET as string,
-        { expiresIn: "7d" }
-    );
-};
